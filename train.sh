d142337c7878b7350c4f3326a8049d0362a5b540
basile-desjuzeur <basile.desjuzeur@hec.edu>
2023-07-12 20:05:54 +0200

    computer migration

--- train.sh
+++ train.sh
@@ -1,1 +1,3 @@
-yolo task=detect mode=train model=weights/yolov8n.pt data=data.yaml epochs=3 imgsz=640
+yolo task=detect mode=train model=weights/yolov8n.pt data=data.yaml epochs=100 batch=16 imgsz=640
+yolo task=detect mode=train model=weights/yolov8n.pt data=data_sample.yaml epochs=1 batch=4 imgsz=640
+yolo task=detect mode=train model=weights/yolov8n.pt data=data.yaml epochs=100 batch=16 imgsz=640 device=0  optimizer='Adam' cos_lr=True
